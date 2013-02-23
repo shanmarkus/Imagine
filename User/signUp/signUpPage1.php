89d1b9086dfc697ae184f2cbf5e12cd44816c21f
Shan Markus <shanmarkus@gmail.com>
2013-02-23 13:24:11 +0700

    first commit

---
+++ User/signUp/signUpPage1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+         <meta charset="utf-8">
+         <title>Sign up Step One</title>
+         <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+         <link rel="stylesheet" type="text/css" href="../../css/signUp.css">
+ </head>
+ <body class="pageOne">
+
+ 	<div class="pageOneHeader">
+ 		
+ 		<div class="pageOneContainer">
+
+ 			<div class="span5">
+ 			</div> <!-- end empty span 3 -->
+
+ 			<div class="span3">
+ 				<form>
+ 				<input type="button" class="pageOneContainerPublic" name="pageOneContainerPublic" id="pageOneContainerPublic" alt="GO Public!">
+ 				</form>
+ 			</div>
+
+ 			<div class="span3">
+ 				<form>
+ 					<input type="button" class="pageOneContainerNinja" name="pageOneContainerNinja" id="pageOneContainerNinja" alt="Go Ninja!">
+ 				</form>
+ 			</div>
+
+ 			<div class="span1">
+ 				<!-- empty span -->
+ 			</div>
+
+ 		</div> <!-- end container div -->
+
+ 	</div> <!-- end header div -->
+ 	
+ </body>
+ </html>
---
+++ User/signUp/signUpPage2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+         <meta charset="utf-8">
+         <title>Sign up Step One</title>
+         <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
+         <link rel="stylesheet" type="text/css" href="../../css/signUp.css">
+ </head>
+ <body class="pageTwo">
+ 	<div class="pageTwoHeader">
+
+ 		<div class="pageTwoContainer">
+
+ 			<div class="span5">
+ 				<!-- empty span -->
+ 			</div> <!-- end of span 5 -->
+
+ 			<div class="span4 pageTwoContainerBox">
+ 			</div> <!-- end of span 4 -->
+
+ 		</div> <!-- end of container -->
+
+ 	</div> <!-- end of header -->
+
+ </body>
+
+ </html>
