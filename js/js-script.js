93c50f3d6fed1f1a210b3806e8cf7f5c769abcbf
Shan Markus <shanmarkus@gmail.com>
2013-03-10 17:41:44 +0700

    Adding js-script.js and css for image input and form

---
+++ js/js-script.js
@@ -0,0 +1,10 @@
+function getFile(){
+	document.getElementById("upfile").click();
+}
+function sub(obj){
+	var file = obj.value;
+	var fileName = file.split("\\");
+	document.getElementById("uploadbtn").innerHTML = fileName[fileName.length-1];
+	document.myForm.submit();
+	event.preventDefault();
+}
